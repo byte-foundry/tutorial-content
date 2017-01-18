fe8b856f2c72fe854bcd1c21b351a5cef3ef1b19
Julien Barret <ibarretj@gmail.com>
2017-01-18 14:42:45 +0100

    added a second sample course

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import course1 from '../static/course1.md';
 import course1Header from '../static/course1-header.md';
+import course2 from '../static/course2.md';
+import course2Header from '../static/course2-header.md';
 
 export default class TutorialContent {
   constructor() {
@@ -12,19 +14,9 @@ export default class TutorialContent {
         name: 'course1'
       },
       {
-        header: course1Header,
-        content: course1,
-        name: 'course1'
-      },
-      {
-        header: course1Header,
-        content: course1,
-        name: 'course1'
-      },
-      {
-        header: course1Header,
-        content: course1,
-        name: 'course1'
+        header: course2Header,
+        content: course2,
+        name: 'course2'
       }
     ];
   }
